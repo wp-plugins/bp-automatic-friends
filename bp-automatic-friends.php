379f4af9c0713ec1075f2424842db4e46bad200a
stevenkword <stevenkword@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-20 18:22:23 +0000

    2.0.5 - Disables email notifications when creating friendships
    git-svn-id: https://plugins.svn.wordpress.org/bp-automatic-friends/trunk@1029519 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-automatic-friends.php
+++ bp-automatic-friends.php
@@ -4,7 +4,7 @@ Plugin Name: BuddyPress Automatic Friends
 Plugin URI: http://www.stevenword.com/bp-automatic-friends/
 Description: Automatically create and accept friendships for specified users upon new user registration. * Requires BuddyPress
 Text Domain: bp-automatic-friends
-Version: 2.0.4
+Version: 2.0.5
 Author: Steven Word
 Author URI: http://stevenword.com/
 License: GPLv2 or later
@@ -34,8 +34,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 class BPAF_Core {
 
-	const VERSION        = '2.0.4';
-	const REVISION       = '20140827';
+	const VERSION        = '2.0.5';
+	const REVISION       = '20141120';
 	const METAKEY        = 'bpaf_global_friend';
 	const LEGACY_OPTION  = 'skw_bpaf_options';
 	const NONCE          = 'bpaf_nonce';
@@ -162,6 +162,10 @@ class BPAF_Core {
 	function create_friendships( $initiator_user_id ) {
 
 		global $bp;
+
+		// Disable email notifications.  In situations with hundreds of users, this can get SPAMMY fast
+		remove_action( 'friends_friendship_requested', 'friends_notification_new_request', 10 );
+		remove_action( 'friends_friendship_accepted', 'friends_notification_accepted_request', 10 );
 
 		/* Get the user data for the initiatorly registered user. */
 		$initiator_user_info = get_userdata( $initiator_user_id );
